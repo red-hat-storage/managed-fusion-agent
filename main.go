723a648d99f6c4b0fd349dc542945534e4756359
Dhruv Bindra <dbindra@redhat.com>
2023-02-17 23:46:26 +0530

    Update reconciler name to managedFusionDeployment
    Signed-off-by: Dhruv Bindra <dbindra@redhat.com>

--- main.go
+++ main.go
@@ -21,14 +21,12 @@ import (
 	"os"
 
 	"go.uber.org/zap/zapcore"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
+	v1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -36,14 +34,9 @@ import (
 	configv1 "github.com/openshift/api/config/v1"
 	openshiftv1 "github.com/openshift/api/network/v1"
 	operators "github.com/operator-framework/api/pkg/operators/v1alpha1"
-	ovnv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1"
 	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	promv1a1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
-	odfv1a1 "github.com/red-hat-data-services/odf-operator/api/v1alpha1"
-	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v1"
-	ocsv1alpha1 "github.com/red-hat-storage/ocs-operator/api/v1alpha1"
-	v1 "github.com/red-hat-storage/ocs-osd-deployer/api/v1alpha1"
-	"github.com/red-hat-storage/ocs-osd-deployer/controllers"
+	"github.com/red-hat-storage/managed-fusion-agent/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -62,8 +55,6 @@ func addAllSchemes(scheme *runtime.Scheme) {
 
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(ocsv1.AddToScheme(scheme))
-
 	utilruntime.Must(promv1.AddToScheme(scheme))
 
 	utilruntime.Must(promv1a1.AddToScheme(scheme))
@@ -74,13 +65,7 @@ func addAllSchemes(scheme *runtime.Scheme) {
 
 	utilruntime.Must(openshiftv1.AddToScheme(scheme))
 
-	utilruntime.Must(odfv1a1.AddToScheme(scheme))
-
-	utilruntime.Must(ocsv1alpha1.AddToScheme(scheme))
-
 	utilruntime.Must(configv1.AddToScheme(scheme))
-
-	utilruntime.Must(ovnv1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -101,31 +86,20 @@ func main() {
 		Port:               9443,
 		LeaderElection:     enableLeaderElection,
 		LeaderElectionID:   "e0c63ac0.openshift.io",
-		NewCache: cache.BuilderWithOptions(cache.Options{
-			SelectorsByObject: cache.SelectorsByObject{
-				&corev1.Namespace{}: {
-					Label: labels.SelectorFromSet(
-						labels.Set{
-							"managed-by-fusion": "true",
-						},
-					),
-				},
-			},
-		}),
 	})
 	if err != nil {
 		setupLog.Error(err, "Unable to start manager")
 		os.Exit(1)
 	}
 
-	if err = (&controllers.ManagedOCSReconciler{
+	if err = (&controllers.ManagedFusionDeploymentReconciler{
 		Client:                       mgr.GetClient(),
 		UnrestrictedClient:           getUnrestrictedClient(),
-		Log:                          ctrl.Log.WithName("controllers").WithName("ManagedOCS"),
+		Log:                          ctrl.Log.WithName("controllers").WithName("ManagedFusionDeployment"),
 		Scheme:                       mgr.GetScheme(),
 		CustomerNotificationHTMLPath: "templates/customernotification.html",
 	}).SetupWithManager(mgr, nil); err != nil {
-		setupLog.Error(err, "Unable to create controller", "controller", "ManagedOCS")
+		setupLog.Error(err, "Unable to create controller", "controller", "ManagedFusionDeployment")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
