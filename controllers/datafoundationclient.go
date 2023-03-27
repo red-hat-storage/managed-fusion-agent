13ed89566ff5ea4cd1b81f16649fead4b2b7206f
Kaustav Majumder <kmajumde@redhat.com>
2023-03-27 12:52:13 +0530

    Added DataFoundation Client packages and controller
    Signed-off-by: Kaustav Majumder <kmajumde@redhat.com>

---
+++ controllers/datafoundationclient.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	v1alpha1 "github.com/red-hat-storage/managed-fusion-agent/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+)
+
+type dataFoundationClientSpec struct {
+	onboardingTicket  string
+	storageDFEndpoint string
+}
+
+type dataFoundationClientReconciler struct {
+	*ManagedFusionOfferingReconciler
+
+	dataFoundationClientSpec dataFoundationClientSpec
+}
+
+func dfCSetupWatches(controllerBuilder *builder.Builder) {}
+
+func DFCAddToScheme(scheme *runtime.Scheme) {
+}
+
+func dfcReconcile(offeringReconciler *ManagedFusionOfferingReconciler, offering *v1alpha1.ManagedFusionOffering) error {
+	return nil
+}
+
+func (r *dataFoundationClientReconciler) initReconciler(offeringReconciler *ManagedFusionOfferingReconciler) {
+}
+
+func (r *dataFoundationClientReconciler) parseSpec(offering *v1alpha1.ManagedFusionOffering) error {
+	return nil
+}
+
+func (r *dataFoundationClientReconciler) reconcileStorageClient() error {
+	return nil
+}
+
+func (r *dataFoundationClientReconciler) reconcileStorageClassClaim() error {
+	return nil
+}
