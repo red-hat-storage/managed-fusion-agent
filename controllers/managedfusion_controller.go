79309d6041d8e3dc5e5b6d2a16c3f083cd2e47e8
Dhruv Bindra <dbindra@redhat.com>
2023-03-28 22:53:38 +0530

    Add reconcileEgress functions for SDN and OVN
    Signed-off-by: Dhruv Bindra <dbindra@redhat.com>

--- controllers/managedfusion_controller.go
+++ controllers/managedfusion_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"strings"
 
 	configv1 "github.com/openshift/api/config/v1"
@@ -41,6 +42,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	"github.com/go-logr/logr"
+	openshiftv1 "github.com/openshift/api/network/v1"
+	ovnv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1"
 	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	promv1a1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
 	"github.com/red-hat-storage/managed-fusion-agent/templates"
@@ -68,6 +71,8 @@ const (
 	monLabelValue                    = "managed-fusion-agent"
 	amSecretPagerDutyServiceKeyKey   = "pagerDutyServiceKey"
 	amSecretSMTPAuthPasswordKey      = "smtpAuthPassword"
+	egressNetworkPolicyName          = "egress-rule"
+	egressFirewallName               = "default"
 )
 
 type ManagedFusionReconciler struct {
@@ -92,6 +97,8 @@ type ManagedFusionReconciler struct {
 	managedFusionSecret          *corev1.Secret
 	smtpConfigData               *smtpConfig
 	pagerDutyConfigData          *pagerDutyConfig
+	egressNetworkPolicy          *openshiftv1.EgressNetworkPolicy
+	egressFirewall               *ovnv1.EgressFirewall
 }
 
 type smtpConfig struct {
@@ -159,7 +166,7 @@ func (r *ManagedFusionReconciler) SetupWithManager(mgr ctrl.Manager, ctrlOptions
 			}}
 		},
 	)
-	return ctrl.NewControllerManagedBy(mgr).
+	ctrlBuilder := ctrl.NewControllerManagedBy(mgr).
 		WithOptions(*ctrlOptions).
 		For(&corev1.Secret{}, managedFusionSecretPredicates).
 		// Watch owned resources
@@ -185,8 +192,15 @@ func (r *ManagedFusionReconciler) SetupWithManager(mgr ctrl.Manager, ctrlOptions
 			&source.Kind{Type: &promv1.PrometheusRule{}},
 			enqueueManagedFusionAgentRequest,
 			monResourcesPredicates,
-		).
-		Complete(r)
+		)
+
+	if r.AvailableCRDs[EgressFirewallCRD] {
+		ctrlBuilder = ctrlBuilder.Owns(&ovnv1.EgressFirewall{})
+	}
+	if r.AvailableCRDs[EgressNetworkPolicyCRD] {
+		ctrlBuilder = ctrlBuilder.Owns(&openshiftv1.EgressNetworkPolicy{})
+	}
+	return ctrlBuilder.Complete(r)
 }
 
 // Reconcile changes to all owned resource based on the infromation provided by the ManangedFusion secret resource
@@ -269,6 +283,14 @@ func (r *ManagedFusionReconciler) initReconciler(ctx context.Context, req ctrl.R
 	r.alertRelabelConfigSecret.Name = alertRelabelConfigSecretName
 	r.alertRelabelConfigSecret.Namespace = r.Namespace
 
+	r.egressNetworkPolicy = &openshiftv1.EgressNetworkPolicy{}
+	r.egressNetworkPolicy.Name = egressNetworkPolicyName
+	r.egressNetworkPolicy.Namespace = r.Namespace
+
+	r.egressFirewall = &ovnv1.EgressFirewall{}
+	r.egressFirewall.Name = egressFirewallName
+	r.egressFirewall.Namespace = r.Namespace
+
 	r.smtpConfigData = &smtpConfig{}
 	r.pagerDutyConfigData = &pagerDutyConfig{}
 }
@@ -327,6 +349,13 @@ func (r *ManagedFusionReconciler) reconcilePhases() (reconcile.Result, error) {
 		if err := r.reconcilePrometheusProxyNetworkPolicy(); err != nil {
 			return ctrl.Result{}, err
 		}
+		if err := r.reconcileEgressFirewall(); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.reconcileEgressNetworkPolicy(); err != nil {
+			return ctrl.Result{}, err
+		}
+
 	}
 
 	return ctrl.Result{}, nil
@@ -692,6 +721,149 @@ func (r *ManagedFusionReconciler) reconcilePrometheusProxyNetworkPolicy() error 
 		return fmt.Errorf("Failed to update prometheus proxy NetworkPolicy: %v", err)
 	}
 	return nil
+}
+
+func (r *ManagedFusionReconciler) reconcileEgressFirewall() error {
+	if !r.AvailableCRDs[EgressFirewallCRD] {
+		r.Log.Info("EgressFirewall CRD not present, skipping reconcile for egress firewall")
+		return nil
+	}
+
+	_, err := ctrl.CreateOrUpdate(r.ctx, r.Client, r.egressFirewall, func() error {
+		if err := r.own(r.egressFirewall); err != nil {
+			return err
+		}
+		desiredEgressSpec, err := r.getEgressFirewallDesiredState()
+		if err != nil {
+			return err
+		}
+		r.egressFirewall.Spec = *desiredEgressSpec
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to update egressFirewall: %v", err)
+	}
+	return nil
+
+}
+
+func (r *ManagedFusionReconciler) reconcileEgressNetworkPolicy() error {
+	if !r.AvailableCRDs[EgressNetworkPolicyCRD] {
+		r.Log.Info("EgressNetworkPolicy CRD not present, skipping reconcile for egress network policy")
+		return nil
+	}
+	_, err := ctrl.CreateOrUpdate(r.ctx, r.Client, r.egressNetworkPolicy, func() error {
+		if err := r.own(r.egressNetworkPolicy); err != nil {
+			return err
+		}
+		desiredEgressFirewallSpec, err := r.getEgressFirewallDesiredState()
+		if err != nil {
+			return err
+		}
+
+		r.egressNetworkPolicy.Spec = *egressFirewallSpecToEgressNetworkPolicySpec(desiredEgressFirewallSpec)
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to update egressNetworkPolicy: %v", err)
+	}
+	return nil
+}
+
+func (r *ManagedFusionReconciler) getEgressFirewallDesiredState() (*ovnv1.EgressFirewallSpec, error) {
+	allowEgressRules := []ovnv1.EgressFirewallRule{}
+	denyEgressRules := []ovnv1.EgressFirewallRule{}
+
+	desired := templates.EgressFirewallTemplate.DeepCopy()
+	for i := range desired.Spec.Egress {
+		egressRule := desired.Spec.Egress[i]
+		switch egressRule.Type {
+		case ovnv1.EgressFirewallRuleAllow:
+			allowEgressRules = append(allowEgressRules, egressRule)
+		case ovnv1.EgressFirewallRuleDeny:
+			denyEgressRules = append(denyEgressRules, egressRule)
+		}
+	}
+
+	// Get the aws config map
+	imdsConfigMap := &corev1.ConfigMap{}
+	imdsConfigMap.Name = utils.IMDSConfigMapName
+	imdsConfigMap.Namespace = r.Namespace
+	if err := r.get(imdsConfigMap); err != nil {
+		return nil, fmt.Errorf("unable to get AWS IMDS ConfigMap: %v", err)
+	}
+
+	// Get the machine cidr
+	vpcCIDR, ok := imdsConfigMap.Data[utils.CIDRKey]
+	if !ok {
+		return nil, fmt.Errorf("unable to determine machine CIDR from AWS IMDS ConfigMap")
+	}
+	// Allow egress traffic to that cidr range
+	cidrList := strings.Split(vpcCIDR, ";")
+	for _, cidr := range cidrList {
+		if cidr == "" {
+			continue
+		}
+		allowEgressRules = append(allowEgressRules, ovnv1.EgressFirewallRule{
+			Type: ovnv1.EgressFirewallRuleAllow,
+			To: ovnv1.EgressFirewallDestination{
+				CIDRSelector: cidr,
+			},
+		})
+	}
+
+	smtpURL, err := url.ParseRequestURI(r.smtpConfigData.Endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse SMTP Endpoint url: %v", err)
+	}
+	if smtpURL.Hostname() == "" {
+		return nil, fmt.Errorf("smtp config does not contain a host entry")
+	}
+
+	smtpEgressRule := ovnv1.EgressFirewallRule{}
+	smtpEgressRule.To.DNSName = smtpURL.Hostname()
+	smtpEgressRule.Type = ovnv1.EgressFirewallRuleAllow
+
+	allowEgressRules = append(
+		allowEgressRules,
+		smtpEgressRule,
+	)
+
+	// The order or rules matter
+	// https://docs.openshift.com/container-platform/4.10/networking/openshift_sdn/configuring-egress-firewall.html#policy-rule-order_openshift-sdn-egress-firewall
+	// Inserting EgressNetworkPolicyRuleAllow rule in the front ensures they come before the EgressNetworkPolicyRuleDeny rule.
+	desired.Spec.Egress = append(allowEgressRules, denyEgressRules...)
+	return &desired.Spec, nil
+}
+
+func egressFirewallSpecToEgressNetworkPolicySpec(firewallSpec *ovnv1.EgressFirewallSpec) *openshiftv1.EgressNetworkPolicySpec {
+	networkPolicySpec := &openshiftv1.EgressNetworkPolicySpec{}
+
+	for i := range firewallSpec.Egress {
+		firewalRule := &firewallSpec.Egress[i]
+		networkPolicyRule := openshiftv1.EgressNetworkPolicyRule{}
+
+		// Convert rule type
+		switch firewalRule.Type {
+		case ovnv1.EgressFirewallRuleAllow:
+			networkPolicyRule.Type = openshiftv1.EgressNetworkPolicyRuleAllow
+		case ovnv1.EgressFirewallRuleDeny:
+			networkPolicyRule.Type = openshiftv1.EgressNetworkPolicyRuleDeny
+		}
+
+		// Convert Rule Peer
+		networkPolicyRule.To = openshiftv1.EgressNetworkPolicyPeer{
+			CIDRSelector: firewalRule.To.CIDRSelector,
+			DNSName:      firewalRule.To.DNSName,
+		}
+
+		networkPolicySpec.Egress = append(
+			networkPolicySpec.Egress,
+			networkPolicyRule,
+		)
+	}
+
+	return networkPolicySpec
 }
 
 func (r *ManagedFusionReconciler) initiateAgentUninstallation() error {
